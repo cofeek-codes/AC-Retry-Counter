953c15c50f61cddc4027a4682feae47ff6e7cacb
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2024-09-29 02:33:10 +0500

    working on going back to pits

--- RetryCounter.lua
+++ RetryCounter.lua
@@ -4,22 +4,31 @@ local prevLapCount = ac.getCar(0).lapCount
 function script.windowMain(dt)
  ui.text('clear lap try: ' .. clearLapTry)
  ui.text('clear laps completed: ' .. ac.getCar(0).lapCount)
- -- width: 150
- -- height: 100
+ -- print(ac.getCar().collidedWith)
+ -- print(physics.teleportCarTo(ac.SpawnSet.Pits))
 end
 
 function script.update(dt)
     local currentLapCount = ac.getCar(0).lapCount
-    
+    -- lap counter
     if currentLapCount > prevLapCount then
         clearLapTry = 1
         prevLapCount = currentLapCount
+    end
+    -- collisionDetection
+    if ac.getCar().collidedWith ~= -1 then
+       -- @TODO: add optional `restart on cars collision`
+       ac.log('collision happened')
+       -- physics.teleportCarTo(ac.SpawnSet.Pits)
     end
 end
 
 ac.onConsoleInput(function(msg)
   if msg == 'rhelp' then
     ac.console('**help message from RetryCounter**')
+  end
+  if msg == 'tp' then
+     physics.teleportCarTo(ac.SpawnSet.Pits)
   end
 end)
 
