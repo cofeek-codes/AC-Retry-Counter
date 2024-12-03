aa12ea63fcfc9d33b09fd40b794f482735b82851
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2024-12-03 19:50:21 +0500

    tries on previous lap implemented

--- RetryCounter.lua
+++ RetryCounter.lua
@@ -1,10 +1,14 @@
 local clearLapTry = 1
 local prevLapCount = ac.getCar(0).lapCount
 local isRetryOnCollision = false
+local triesOnPrevLap = 0
+
 
 function script.windowMain(dt)
  ui.text('clear lap try: ' .. clearLapTry)
  ui.text('clear laps completed: ' .. ac.getCar(0).lapCount)
+ ui.text('tries on last clear lap: ' .. triesOnPrevLap)
+
  retryOnCollisionCheckbox = ui.checkbox('Retry on collision', isRetryOnCollision)
 end
 
@@ -12,6 +16,8 @@ function script.update(dt)
     local currentLapCount = ac.getCar(0).lapCount
     -- lap counter
     if currentLapCount > prevLapCount then
+      -- clear lap happened
+      triesOnPrevLap = clearLapTry
         clearLapTry = 1
         prevLapCount = currentLapCount
     end
